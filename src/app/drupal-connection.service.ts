9579caa933d0c7aefee278473a574f9e39d37d55
Ashley Maher <ashley.maher@didymodesigns.com.au>
2018-10-18 15:34:56 +1100

    The Angular Application needs to communicate with the Drupal Backend. This service provides the connection points and values for the communication to take place.

---
+++ src/app/drupal-connection.service.ts
@@ -0,0 +1,66 @@
+import {Injectable} from '@angular/core';
+import {AppData} from './app-data';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+/**
+ * The Angular Application is being launched as a modal above the event page created in Drupal.
+ * The Angular Application needs to communicate with the Drupal backend.
+ * This service creates all the connections points to be used by other services and components in the Angular Application
+ */
+export class DrupalConnectionService {
+
+  public apiGetEventState:      string;
+  public apiGetTheQuestions:    string;
+  public apiPostTheQuestions:   string;
+  public apiGetTheQuestionnaireResults: string;
+  public apiPostGetTmp:         string; // TMP stands for Traffic Management Plan, used to both POST and GET
+  public apiTmpBounds:          string; // TMP stands for Traffic Management Plan, used to both POST and GET
+                                        // bounds are defined by both two Latitude and two Longitude.
+  public apiTmpObjects:         string; // The icon positions for the barriers etc for the traffic management plan
+
+  public api: string;
+  public postapi: string;
+  public getapi: string;
+
+  /**
+   * the csrfToken is a cross site scripting attack token.
+   */
+  public csrfToken: string;
+
+  /**
+   *
+   * @param http
+   * @param appData
+   */
+  constructor(private http: HttpClient, private appData: AppData) {
+
+    this.apiGetEventState =   'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/state?_format=json';
+    this.apiGetTheQuestions = 'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/questionnaire/result?_format=json';
+    this.apiPostTheQuestions = 'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/questionnaire/submit?_format=json';
+    this.apiGetTheQuestionnaireResults = 'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/questionnaire/result?_format=json';
+    this.apiPostGetTmp =      'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/tmp?_format=json';
+    this.apiTmpBounds =       'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/tmp/bounds?_format=json';
+    this.apiTmpObjects =      'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/tmp/objects?_format=json';
+
+    /**
+     * the below is the original set which need to be removed
+     */
+    this.api = 'http://' + this.appData.baseurl + '/questionnaire/questions?_format=json';
+    this.postapi = 'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/questionnaire/submit?_format=json';
+    this.getapi = 'http://' + this.appData.baseurl + '/event/' + this.appData.eventid + '/questionnaire/result?_format=json';
+
+    /**
+     * 
+     */
+    http.get('http://' + this.appData.baseurl + '/rest/session/token', {responseType: 'text'})
+      .subscribe((value) => {
+        console.log(value);
+        this.csrfToken = value;
+      });
+  }
+}
