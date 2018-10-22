eef7ab3d8278ca4ba296c35eb468adbb16613d86
Ashley Maher <ashley.maher@didymodesigns.com.au>
2018-10-22 15:27:32 +1100

    The boolean logi was reversed. THe logic is now correct.

--- src/app/question-show/question-show.component.ts
+++ src/app/question-show/question-show.component.ts
@@ -5,8 +5,6 @@ import {TrafficPlanCategory} from '../traffic-plan-category';
 import {ClassResponse} from '../traffic-plan-category';
 
 
-
-
 // @ts-ignore
 @Component({
   selector: 'app-question-show',
@@ -36,15 +34,16 @@ export class QuestionShowComponent implements OnInit {
     private questionService: QuestionsService,
   ) {
   }
+
   ngOnInit(): void {
     this.radioButtons = [0];
     this.classReturnInformation = new ClassResponse();
     this.getQuestion();
   }
 
- /* eventNu() {
-    this.activeIndexChange.emit(1);
-  }*/
+  /* eventNu() {
+     this.activeIndexChange.emit(1);
+   }*/
   getQuestion(): void {
     this.questionService.getQuestion().subscribe((results) => this.assignResults(results));
   }
@@ -61,25 +60,17 @@ export class QuestionShowComponent implements OnInit {
   private assignCategory(results) {
     this.categoryInformation = results;
   }
+
   public onSubmit(value) {
-    console.log('before');
-    // alert('in onsubmit');
+
     this.makePOSTJsonStringBody(value);
     this.activeIndexChange.emit(1);
     this.questionService.postAnswers(this.postBody).subscribe(
-      //(response) => this.classReturn(response)
     );
-    console.log(this.postBody);
-    console.log('test');
+
     event.preventDefault();
   }
 
-
- /* private classReturn(response) {
-    this.classReturnInformation = response;
-    console.log(response);
-    console.log(this.classReturnInformation);
-  }*/
 
   /**
    * When the POST function POSTs to the back end the body must be formatted
@@ -87,7 +78,7 @@ export class QuestionShowComponent implements OnInit {
    * @param value
    */
 
-  private makePOSTJsonStringBody (value) {
+  private makePOSTJsonStringBody(value) {
     /**
      * The first return line does not have a comma preceding.
      * This variable tracks if this is the end of the first line
@@ -101,9 +92,9 @@ export class QuestionShowComponent implements OnInit {
     this.postBody = '{';
     for (const entry of this.questions) {
       if (entry.result === '0') {
-        ourBoolean = 'false';
-      } else if (entry.result === '1') {
         ourBoolean = 'true';
+      } else if (entry.result === '1') {
+        ourBoolean = 'false';
       }
       if (first) {
         this.postBody = this.postBody + ',';
@@ -114,25 +105,15 @@ export class QuestionShowComponent implements OnInit {
       + JSON.stringify(entry.id)}:{${JSON.stringify(entry.questionText)}:${ourBoolean}}`;
 
 
-      // console.log(JSON.stringify(entry));
-      // console.log(JSON.stringify(entry.id));
-      // console.log(JSON.stringify(entry.questionText));
-      // console.log(JSON.stringify(entry.id));
-      // console.log(JSON.stringify(entry.questionText));
-      // console.log(JSON.stringify(entry.result));
-      // this.postBody = this.postBody + '}';
     }
     this.postBody = this.postBody + '}';
-    // console.log(this.postBody);
   }
 
   protected changeRadioButton(value, questionId) {
     console.log(value);
-    // console.log(questionId);
   }
 
   protected processForm() {
-    alert('got the form info');
     console.log('test');
     event.preventDefault();
   }
